72b1f2826d89ac89e5eebc8862f27cdc5fc05274
David Mändlen <david.maendlen@bifroe.st>
2016-10-20 16:28:05 +0200

    fixing error "static root not found"

--- oscmt/oscmt/settings.py
+++ oscmt/oscmt/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 #LOGGING = {
 #    'version': 1,
@@ -148,6 +152,3 @@ STATIC_URL = '/static/'
 #        },
 #    },
 #}
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
